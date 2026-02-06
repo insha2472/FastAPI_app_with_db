8ae7ccf04c4685f9c30bebeacd4155d4458b5ff2
insha2472 <syedainshiraah84@gmail.com>
2026-02-06 15:56:35 +0530

    shidsu

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from db import get_db
+from repositeries.user_repo import UserRepo
+from schemas.user_schemas import UserSchema
 
 router = APIRouter()
 
 @router.get("/signup")
-def signup():
+def signup(db:Session=Depends(get_db)):
+    user_repo = UserRepo(db)
+    user_repo.add_user(user)
     return {"message": "user created"}
 
 @router.get("/login")
